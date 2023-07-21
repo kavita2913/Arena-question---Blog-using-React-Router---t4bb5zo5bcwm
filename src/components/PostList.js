93ebf81644f6f019911ce4f4150b53d92980680c
kavi <116868545+kavita2913@users.noreply.github.com>
2023-07-21 20:06:53 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const posts = [
-    { name: 'Post 1', path: '/posts/1' },
-    { name: 'Post 2', path: '/posts/2' },
-    { name: 'Post 3', path: '/posts/3' },
-    { name: 'Post 4', path: '/posts/4' }
+  { name: 'Post 1', path: '/posts/1' },
+  { name: 'Post 2', path: '/posts/2' },
+  { name: 'Post 3', path: '/posts/3' },
+  { name: 'Post 4', path: '/posts/4' }
 ];
+
 export default function PostList() {
-    return (
-      <div className="posts">
-        <h1>Post List</h1>
-        <ul>
-          {/* Render a list of Links from the posts object   */}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="posts">
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <Link to={post.path}>{post.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
